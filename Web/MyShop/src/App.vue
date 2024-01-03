0cf1828147df042c0644514167bdb61e6861fae5
Neko <jkspstyu@outlook.com>
2024-01-04 00:03:37 +0800

    创建前端项目模块以及配置环境

---
+++ Web/MyShop/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import {RouterView} from "vue-router";
+</script>
+
+<template>
+  <RouterView></RouterView>
+</template>
+
+<style scoped>
+
+</style>
